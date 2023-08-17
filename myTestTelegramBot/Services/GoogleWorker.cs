782310f9d4f5ed771fdf4b34f79eeac0754d1dcb
Maksim <mrmaksoracle89537089833@yandex.ru>
2023-08-17 17:31:05 +0300

    update

--- myTestTelegramBot/Services/GoogleWorker.cs
+++ myTestTelegramBot/Services/GoogleWorker.cs
@@ -8,37 +8,44 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace myTestTelegramBot.Services
 {
-    public class GoogleWorker
+    public static class GoogleWorker
     {
+        private static readonly ClientSecrets clientSecrets = new ClientSecrets
+        {
+            ClientId = "252432177845-q906ditfr1idohcv9lpfkotejapu93vn.apps.googleusercontent.com",
+            ClientSecret = "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"
+        };
+        private const string ApplicationName = "Google Sheets API .NET Quickstart";
+        private const string User = "user";
+        private const string SheetLink = "1MMJnUAx7IFlQB4Yg-bbAypuhlAlIFlRc1eT3cwi4-Jk";
+        private const string SheetName = "tr";
+        private static readonly string SheetRange = SheetName+"!A:E";
+
         public static void Add(TransactionModel transaction)
         {
             try
             {
                 string[] Scopes = { SheetsService.Scope.Spreadsheets };
-                var clientSecrets = new ClientSecrets
-                {
-                    ClientId = "252432177845-q906ditfr1idohcv9lpfkotejapu93vn.apps.googleusercontent.com",
-                    ClientSecret = "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"
-                };
-                var res = GoogleWebAuthorizationBroker.AuthorizeAsync(clientSecrets, Scopes, "user", CancellationToken.None, new FileDataStore("MyAppsToken"));
+
+                var res = GoogleWebAuthorizationBroker.AuthorizeAsync(clientSecrets, Scopes, User, CancellationToken.None, new FileDataStore("MyAppsToken"));
                 var baseClientService = new BaseClientService.Initializer()
                 {
                     HttpClientInitializer = res.Result,
-                    ApplicationName = "Google Sheets API .NET Quickstart"
+                    ApplicationName = ApplicationName
                 };
                 var service = new SheetsService(baseClientService);
 
 
-                SpreadsheetsResource.ValuesResource.GetRequest getRequest = service.Spreadsheets.Values.Get("1MMJnUAx7IFlQB4Yg-bbAypuhlAlIFlRc1eT3cwi4-Jk", "tr!A:E");
+                SpreadsheetsResource.ValuesResource.GetRequest getRequest = service.Spreadsheets.Values.Get(SheetLink, SheetRange);
                 System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate (object sender, X509Certificate certificate, X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors) { return true; };
                 ValueRange getResponse = getRequest.Execute();
                 IList<IList<object>> values = getResponse.Values;
-                var range = $"{"tr"}!A" + (values.Count + 1) + ":E" + (values.Count + 1);
+                var range = $"{""+SheetName+""}!A" + (values.Count + 1) + ":E" + (values.Count + 1);
 
 
                 var valueRange = new ValueRange();
                 valueRange.Values = new List<IList<object>> { new List<object>() { transaction.Date, transaction.Text } };
-                var updateRequest = service.Spreadsheets.Values.Update(valueRange, "1MMJnUAx7IFlQB4Yg-bbAypuhlAlIFlRc1eT3cwi4-Jk", range);
+                var updateRequest = service.Spreadsheets.Values.Update(valueRange, SheetLink, range);
                 updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.USERENTERED;
                 var updateResponse = updateRequest.Execute();
             }
